13a808d6757df51dd73b2064c31198f3609d31b9
Pierre TRETON <pierre.treton@etudiant.univ-rennes1.fr>
2024-09-24 14:15:55 +0200

    project setup

---
+++ src/main/java/org/example/kahootspring/KahootSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.kahootspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahootSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KahootSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/kahootspring/KahootSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kahootspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahootSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
